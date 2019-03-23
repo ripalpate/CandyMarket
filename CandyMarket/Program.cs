b5399c0442ccf1e5650658306a44958256fe3915
unknown <rys.mary@gmail.com>
2019-03-23 10:44:06 -0500

    add candy works

--- CandyMarket/Program.cs
+++ CandyMarket/Program.cs
@@ -9,12 +9,7 @@ namespace CandyMarket
         {
             var db = SetupNewApp();
 
-            var exit = false;
-            while (!exit)
-            {
-                var userInput = MainMenu();
-                exit = TakeActions(db, userInput);
-            }
+            var candyCounter = new List<int> {1234, 1235, 1236};
 
             var myCandy = new Candy("candyYum", "sweetums", FlavorType.HardCandy, DateTime.Now , 1234);
             var myCandy2 = new Candy("sweets", "mars", FlavorType.Sour, DateTime.Now, 1235);
@@ -23,6 +18,14 @@ namespace CandyMarket
             var MainOwner = new Owner("Owner1", new List<Candy> {myCandy});
             var Bob = new Owner("Owner2", new List<Candy> {myCandy2});
             var Daphne = new Owner("Owner3", new List<Candy> {myCandy3});
+
+            var exit = false;
+            while (!exit)
+            {
+                var userInput = MainMenu();
+                exit = TakeActions(db, userInput, MainOwner, myCandy, myCandy2, myCandy3, candyCounter);
+            }
+
 
             foreach (var candy in MainOwner.CandyList)
             {
@@ -50,7 +53,9 @@ namespace CandyMarket
         internal static ConsoleKeyInfo MainMenu()
         {
             View mainMenu = new View()
-                    .AddMenuOption("Did you just get some new candy? Add it here.")
+                    .AddMenuOption("Did you want to add candyYum? Add it here.")
+                    .AddMenuOption("Did you want to add sweets? Add it here.")
+                    .AddMenuOption("Did you want to add coolCandy? Add it here.")
                     .AddMenuOption("Do you want to eat some candy? Take it here.")
                     .AddMenuText("Press Esc to exit.");
             Console.Write(mainMenu.GetFullMenu());
@@ -58,7 +63,7 @@ namespace CandyMarket
             return userOption;
         }
 
-        private static bool TakeActions(CandyStorage db, ConsoleKeyInfo userInput)
+        private static bool TakeActions(CandyStorage db, ConsoleKeyInfo userInput, Owner MainOwner, Candy candyYum, Candy sweets, Candy coolCandy, List<int> candyCounter)
         {
             Console.Write(Environment.NewLine);
 
@@ -69,23 +74,47 @@ namespace CandyMarket
             switch (selection)
             {
                 case "1":
-                    AddNewCandy(db);
+                    AddNewCandy(MainOwner, candyYum, candyCounter);
                     break;
                 case "2":
+                    AddNewCandy(MainOwner, sweets, candyCounter);
+                    break;
+                case "3":
+                    AddNewCandy(MainOwner, coolCandy, candyCounter);
+                    break;
+                case "4":
                     EatCandy(db);
                     break;
-                default: return true;
+                default: return false;
             }
             Console.ReadLine();
-            return true;
+            return false;
         }
 
-        internal static void AddNewCandy(CandyStorage db)
+        public static void AddNewCandy(Owner MainOwner, Candy myCandy, List<int> candyCounter)
         {
-            
+            var dateTime = DateTime.Now;
+            var maxId = 0;
 
-            //var savedCandy = db.SaveNewCandy(newCandy);
-           // Console.WriteLine($"Now you own the candy {savedCandy.Name}");
+            foreach (var Id in candyCounter)
+            {
+                if (Id > maxId)
+                {
+                    maxId = Id;
+                }
+            }
+
+            maxId++;
+            myCandy.RecievedDate = dateTime;
+            myCandy.CandyId = maxId;
+            candyCounter.Add(maxId);
+            MainOwner.CandyList.Add(myCandy);
+            Console.WriteLine($"{myCandy.CandyId}, {myCandy.Name}, {myCandy.RecievedDate}");
+            foreach (var item in MainOwner.CandyList)
+            {
+                Console.WriteLine(item.Name);
+            }
+       
         }
 
         private static void EatCandy(CandyStorage db)
